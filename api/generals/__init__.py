2321ba1023aa7e2c581c5ee182541bc3845126c5
AndresIs <andres.is.rolla@gmail.com>
2025-08-10 21:24:27 -0600

    feat(documents): Add document processing app and update configurations
    Integrated the `document_processing` app into the project by configuring settings, updating URLs, and initializing empty urlpatterns. Introduced `StorageConstants` for better bucket/path management and refined `.env` and `.gitignore` to support the updates. Enhanced project modularity by restructuring imports and logical constants.

---
+++ api/generals/__init__.py
@@ -0,0 +1,7 @@
+from generals.loggers import AuraTestLogger
+
+from generals.abstract_models import RecordHistoryAbstractModel
+from generals.constants import StorageConstants
+
+
+__all__ = ["RecordHistoryAbstractModel", "AuraTestLogger", "StorageConstants"]
